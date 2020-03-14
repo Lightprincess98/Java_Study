2efb224e712dd69645445e05375dee3fe152c3d9
Alexis Stahnke <alexis.stahnke@protonmail.com>
2020-03-14 14:39:47 +0200

    First Initial Files of Studying
    The first files I started with

---
+++ Java Study/MyClass.java
@@ -0,0 +1,10 @@
+public class MyClass {
+  public static void main(String[] args) {
+    int x = 5;
+    x &= 4;
+    System.out.println(x);
+
+    String txt = "Please locate where 'locate' occurs!";
+System.out.println(txt.indexOf("l")); // Outputs 7
+  }
+}
